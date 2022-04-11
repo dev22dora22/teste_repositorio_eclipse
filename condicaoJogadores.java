fa2ad709c6740f7f93526e8a3c5eb8ad17abd328
Isa R.Silva <92555194+dev22dora22@users.noreply.github.com>
2022-04-11 18:38:51 -0300

    Condição - Idade de jogadores
    Condição. Jogadores.

---
+++ condicaoJogadores.java
@@ -0,0 +1,16 @@
+package umContador;
+
+public class condicaoJogadores {
+
+	public static void main(String[]args) {
+       int idadeDeJogadores = 22 + 24 + 26 + 28;
+       int idadeDeJogadoras = 23 + 22 + 21 + 26;
+       System.out.println(idadeDeJogadores + idadeDeJogadoras);
+	
+if (idadeDeJogadores >= idadeDeJogadoras) {
+	System.out.println("Eles podem competir!");
+} else {
+	System.out.println("Eles não podem competir!");
+  
+
+	
